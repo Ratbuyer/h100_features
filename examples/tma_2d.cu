f61cc47c75c42c4750bbfa567af9d4c25c536e0e
Yicong Eddy Luo <m1662576219@gmail.com>
2024-07-23 05:09:17 -0400

    update

--- examples/tma_2d.cu
+++ examples/tma_2d.cu
@@ -70,7 +70,7 @@ __global__ void test(const __grid_constant__ CUtensorMap global_fake_tensor_map,
   // Write back to global memory:
   if (threadIdx.x == 0)
   {
-    cde::cp_async_bulk_tensor_2d_shared_to_global(&global_fake_tensor_map, base_j, base_i, smem_buffer);
+    cde::cp_async_bulk_tensor_2d_shared_to_global(&global_fake_tensor_map, base_i, base_j, smem_buffer);
     cde::cp_async_bulk_commit_group();
     cde::cp_async_bulk_wait_group_read<0>();
   }
