50401ca1ec17e8da217d9b1839e58648bbb5595d
Yicong Eddy Luo <m1662576219@gmail.com>
2024-07-24 01:23:55 -0400

    update

--- examples/wgmma_sparse.cu
+++ examples/wgmma_sparse.cu
@@ -78,21 +78,21 @@ __global__ void kernel(half *A, half *B, half *C, u_int32_t *metadata_array)
 
   asm volatile("wgmma.fence.sync.aligned; \n");
 
-  // asm volatile("wgmma.mma_async.sp.sync.aligned.m64n8k32.f16.f16.f16 "
-  //            "{%0, %1}, " // c
-  //            "%2, %3, "   // desc A, B
-  //            "%4, "       // meta
-  //            "0, "       // thread selection
-  //            "1, "       // scale D
-  //            "%7, %8, "   // +/- scale A, B
-  //            "%9, %10;"   // transpose A, B
-  //            : "+r"(c[0]), "+r"(c[1])
-  //            : "l"(desc_a), "l"(desc_b),
-  //              "r"(metadata),   // metadata
-  //              "r"(0),        // thread selection
-  //              "r"(1),          // scale D
-  //              "n"(1), "n"(1),  // +- scale A, B
-  //              "n"(0), "n"(1)); // transpose A, B
+  asm volatile("wgmma.mma_async.sp.sync.aligned.m64n8k32.f16.f16.f16 "
+             "{%0, %1}, " // c
+             "%2, %3, "   // desc A, B
+             "%4, "       // meta
+             "0, "       // thread selection
+             "1, "       // scale D
+             "%7, %8, "   // +/- scale A, B
+             "%9, %10;"   // transpose A, B
+             : "+r"(c[0]), "+r"(c[1])
+             : "l"(desc_a), "l"(desc_b),
+               "r"(metadata),   // metadata
+               "r"(0),        // thread selection
+               "r"(1),          // scale D
+               "n"(1), "n"(1),  // +- scale A, B
+               "n"(0), "n"(1)); // transpose A, B
 
   asm volatile("wgmma.commit_group.sync.aligned; \n");
 
