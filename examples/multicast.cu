0c7377d87094b3ded0f6229ba02d16a530c24fa6
Yicong Eddy Luo <m1662576219@gmail.com>
2024-07-24 02:29:45 -0400

    update

--- examples/multicast.cu
+++ examples/multicast.cu
@@ -87,7 +87,9 @@ __global__ void __cluster_dims__(2, 1, 1) kernel(const __grid_constant__ CUtenso
     printf("\n");
   }
 
-    if (clusterBlockRank == 0 && threadIdx.x == 0)
+  cluster.sync();
+
+  if (clusterBlockRank == 0 && threadIdx.x == 0)
   {
     printf("clusterBlockRank: %d, threadIdx.x: %d\n", clusterBlockRank, threadIdx.x);
     for (int i = 0; i < tile_size; ++i)
